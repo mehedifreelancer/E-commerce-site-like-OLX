57af23943398cfba9567a678cd29479cce028b0c
mehedifreelancer <mehedifreelancer452@gmail.com>
2019-03-16 10:06:29 +0600

    E-commerce site
    It is e-commerce website like olx

---
+++ js/myjs.js
@@ -0,0 +1,42 @@
+
+
+
+$(".btn-group-ct a").click(function () {
+    $(".fa-arrow-down").css({
+        "transform":"rotate(180deg)"
+    })
+    $(".cat-btn").css({
+        "display":'none'
+    })
+    $(".cat-btn-two").css({
+        "display":'block'
+    })
+
+    //Condition for div options fadeIn
+    $(".options").slideDown('slow')
+
+
+})
+
+$(".cat-btn-two, .navbarPlace,.components, .banner-image img").click(function () {
+    $(".fa-arrow-down").css({
+        "transform":"rotate(0deg)"
+    })
+    $(".cat-btn-two").css({
+        "display":'none'
+    })
+    $(".cat-btn").css({
+        "display":'block'
+    })
+    $(".options").slideUp('slow')
+})
+
+
+
+/*Code for start selling menu */
+
+$(".manual").click(function () {
+    $(".location-input").css({
+        "display":"block"
+    })
+})
